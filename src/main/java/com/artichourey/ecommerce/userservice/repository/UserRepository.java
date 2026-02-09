3d390ab891b8f538bdcff1a25023127825f79997
Arti <arti.chourey21@gmail.com>
2026-02-09 15:15:15 +0530

    feat: add user repository

---
+++ src/main/java/com/artichourey/ecommerce/userservice/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.artichourey.ecommerce.userservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.artichourey.ecommerce.userservice.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+	boolean existsByEmail(String email);
+	Optional<User> findByEmail( String email);
+}
