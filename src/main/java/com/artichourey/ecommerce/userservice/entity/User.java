b8c2085303254766b2d904c3b20d4571b8271e79
Arti <arti.chourey21@gmail.com>
2026-02-26 20:29:08 +0530

    fix: handle H2 reserved keyword conflict for User entity

--- src/main/java/com/artichourey/ecommerce/userservice/entity/User.java
+++ src/main/java/com/artichourey/ecommerce/userservice/entity/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +21,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name="users")
 public class User {
 	
 	@Id
