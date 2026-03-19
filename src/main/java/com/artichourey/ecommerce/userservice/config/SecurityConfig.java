4e2d18930941a92493d23c7b3c50cfec7f6099df
Arti <arti.chourey21@gmail.com>
2026-03-19 20:50:11 +0530

    refactor:change the security config as per API Gateway

--- src/main/java/com/artichourey/ecommerce/userservice/config/SecurityConfig.java
+++ src/main/java/com/artichourey/ecommerce/userservice/config/SecurityConfig.java
@@ -3,51 +3,28 @@ package com.artichourey.ecommerce.userservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.artichourey.ecommerce.userservice.util.JwtAuthenticationFilter;
-
 
 
 @Configuration
-
 public class SecurityConfig {
 
-	private final JwtAuthenticationFilter jwtAuthenticationFilter;
-	
-	
-	
-	
-	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-            
-            this.jwtAuthenticationFilter=jwtAuthenticationFilter;
-               }
-
-	
-	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new  BCryptPasswordEncoder();
 		
 	}
+	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		http.csrf(csrf->csrf.disable())
-		.sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		.authorizeHttpRequests(auth->auth.requestMatchers("/api/users/register","/api/users/login",
-	          
-	            "/v3/api-docs/**",        
-	            "/swagger-ui/**",         
-	            "/swagger-ui.html").permitAll()
-				
-				.anyRequest().authenticated()).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		
-		return http.build();
-		
-	}
-	
-	
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() 
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
 }
+
+	
+
